99fcd8410e443999c4d273dc22b14c2def2873d2
mcelearr <rorymcelearney@gmail.com>
2016-08-15 09:11:10 +0100

    sets up post detail page to display class

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,16 +1,27 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
+from .models import Post
 
 def post_create(request):
     return HttpResponse("<h1>Create</h1>")
 
 def post_detail(request):
-    return HttpResponse("<h1>Detail</h1>")
+    instance = get_object_or_404(Post, id=1)
+    context = {
+        "title": "Detail",
+        "instance": instance,
+    }
+    return render(request, "post_detail.html", context)
 
 def post_list(request):
-    return render(request, "index.html", {})
+    queryset = Post.objects.all()
+    context = {
+        "object_list": queryset,
+        "title": "List"
+    }
+    return render(request, "index.html", context)
 
 def post_update(request):
     return HttpResponse("<h1>Update</h1>")
